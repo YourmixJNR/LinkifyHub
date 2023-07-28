bfa4db922a17195ea6e828658722a192a219968e
Michael Victor <radmakson@gmail.com>
2023-07-28 19:45:51 +0100

    done

--- js/fileUpload.js
+++ js/fileUpload.js
@@ -16,9 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
         filePlaceHolder.textContent = '';
     } else {
         fileNameElement.textContent = '';
+        uploadedUrl.textContent = ''; // Clear the previously displayed URL when selecting a new file
     }
 
-    uploadedUrl.textContent = ''; // Clear the previously displayed URL when selecting a new file
   });
 
   uploadButton.addEventListener('click', async () => {
@@ -50,6 +50,31 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Error:', error);
         alert('File upload failed. Please try again later.');
     }
+  });
 });
-});
+
+function copyText() {
+    const filetUrl = document.getElementById('uploaded-url').value;
   
+    if (filetUrl === '') {
+      alert("Can't copy empty field");
+      return;
+    }
+  
+    const inputField = document.createElement('input');
+    inputField.value = filetUrl;
+    document.body.appendChild(inputField);
+  
+    inputField.select();
+    inputField.setSelectionRange(0, 99999); // For mobile devices
+  
+    try {
+      const successful = document.execCommand('copy');
+      document.body.removeChild(inputField); // Remove the temporary input field
+      const message = successful ? 'Copied to clipboard!' : 'Copy failed. Please select and copy manually.';
+      alert(message);
+    } catch (error) {
+      console.error('Copy failed:', error);
+      alert('Copy failed. Please select and copy manually.');
+    }
+  };
