4929a9f4eaec8cec9ff7f258913e80b09f539295
Michael Victor <radmakson@gmail.com>
2023-07-28 10:56:44 +0100

    file: test

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         headers: headers,
         body: data
       });
+
       const result = await response.json();
       const shortUrl = result.url;
       shortUrlInput.value = shortUrl;
@@ -40,7 +41,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  
+  const fileInput = document.getElementById('file-input');
+  const fileNameElement = document.getElementById('file-name');
+  const filePlaceHolder = document.getElementById('file-palaceholder');
+
+  fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+
+    if (file) {
+      // Update the custom browse button content with the file details
+      fileNameElement.textContent = `${file.name}`;
+      filePlaceHolder.textContent = '';
+    } else {
+      // If no file selected, display the default message
+      fileNameElement.textContent = '';
+    }
+  });
 });
 
 function copyText() {
