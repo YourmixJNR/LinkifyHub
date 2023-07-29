fd846068f7ba5101c9915f4db0f8cbff6a670989
Michael Victor <radmakson@gmail.com>
2023-07-29 08:33:04 +0100

    bugs

--- js/fileUpload.js
+++ js/fileUpload.js
@@ -59,28 +59,28 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-function copyText() {
-    const filetUrl = document.getElementById('uploaded-url').value;
-  
-    if (filetUrl === '') {
-      alert("Can't copy empty field");
-      return;
+function copyFileUrl() {
+    const fileUrl = document.getElementById('uploaded-url').value;
+
+    if (fileUrl === '') {
+        alert("Can't copy empty field");
+        return;
     }
-  
-    const inputField = document.createElement('input');
-    inputField.value = filetUrl;
-    document.body.appendChild(inputField);
-  
-    inputField.select();
-    inputField.setSelectionRange(0, 99999); // For mobile devices
-  
+
+    const fileUrlField = document.createElement('input');
+    fileUrlField.value = fileUrl;
+    document.body.appendChild(fileUrlField);
+
+    fileUrlField.select();
+    fileUrlField.setSelectionRange(0, 99999); // For mobile devices
+
     try {
-      const successful = document.execCommand('copy');
-      document.body.removeChild(inputField); // Remove the temporary input field
-      const message = successful ? 'Copied to clipboard!' : 'Copy failed. Please select and copy manually.';
-      alert(message);
+        const successful = document.execCommand('copy');
+        document.body.removeChild(fileUrlField); // Remove the temporary input field
+        const message = successful ? 'Copied to clipboard!' : 'Copy failed. Please select and copy manually.';
+        alert(message);
     } catch (error) {
-      console.error('Copy failed:', error);
-      alert('Copy failed. Please select and copy manually.');
+        console.error('Copy failed:', error);
+        alert('Copy failed. Please select and copy manually.');
     }
-  };
+}
