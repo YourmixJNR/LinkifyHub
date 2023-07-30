0eb1bf8e7457a323e1ac25d21625b38ab53759a7
Michael Victor <radmakson@gmail.com>
2023-07-30 07:05:24 +0100

    mod

--- js/imageVideo.js
+++ js/imageVideo.js
@@ -2,28 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const apiKey = '70f72cd3748a0afd4ddbf55df9d2376c1e16174b'; // Replace 'YOUR_API_KEY' with your actual API key
         const apiUrl = 'https://extractorapi.com/api/v1/extractor';
 
-        const shortenButton = document.getElementById('shorten-button');
-        const longUrlInput = document.getElementById('long-url');
-        const shortUrlInput = document.getElementById('short-url');
+        const extractButton = document.getElementById('extract-button');
+        const urUrlInput = document.getElementById('ur-url');
+        const resultsUrlDisplay = document.getElementById('results-url');
 
-        shortenButton.addEventListener('click', async (event) => {
+        extractButton.addEventListener('click', async (event) => {
         event.preventDefault();
-        const longUrl = longUrlInput.value.trim();
+        const urUrl = urUrlInput.value.trim();
 
         const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
 
-        if (longUrl === '' || !urlRegex.test(longUrl)) {
+        if (urUrl === '' || !urlRegex.test(urUrl)) {
             alert('Please enter a valid URL.');
             return;
         }
 
-        const endpoint = apiUrl + '?apikey=' + apiKey + '&url=' + encodeURIComponent(longUrl);
+        const endpoint = apiUrl + '?apikey=' + apiKey + '&url=' + encodeURIComponent(urUrl);
 
         try {
             const response = await fetch(endpoint);
             const result = await response.json();
-            const shortUrl = result.text;
-            shortUrlInput.textContent = shortUrl;
+            const resultsUrl = result.images;
+            resultsUrlDisplay.textContent = resultsUrl;
         } catch (error) {
             console.error('Error:', error);
         }
